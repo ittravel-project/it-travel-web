56c38e6fb4b4cf6cb1b03e30af9c15ddf0775531
RedBalloon24 <worldtraveler757@gmail.com>
2019-07-20 19:36:22 +0200

    mvp 6

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { AuthStore } from './contexts/AuthStore';
 
 
 ReactDOM.render(
-<BrowserRouter>
+<BrowserRouter basename={process.env.PUBLIC_URL}>
     <AuthStore>
         <App />
     </AuthStore>
