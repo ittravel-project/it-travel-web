add81294cd0dfb116a092245b7ff782455b27bee
RedBalloon24 <worldtraveler757@gmail.com>
2019-07-20 19:03:32 +0200

    Updates

---
+++ precache-manifest.1009fefd26ef821ecb41bb185e74b9ec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725047e82fd2d2fc3f233c236b88f7ba",
+    "url": "/ittravel-project/it-travel-web/index.html"
+  },
+  {
+    "revision": "aafc43b14a998a631d94",
+    "url": "/ittravel-project/it-travel-web/static/css/2.999a9761.chunk.css"
+  },
+  {
+    "revision": "f14437162207a4d2009b",
+    "url": "/ittravel-project/it-travel-web/static/css/main.eb500b8d.chunk.css"
+  },
+  {
+    "revision": "aafc43b14a998a631d94",
+    "url": "/ittravel-project/it-travel-web/static/js/2.1d9ce079.chunk.js"
+  },
+  {
+    "revision": "f14437162207a4d2009b",
+    "url": "/ittravel-project/it-travel-web/static/js/main.97c5bae1.chunk.js"
+  },
+  {
+    "revision": "ca1a0bdf7986bd605540",
+    "url": "/ittravel-project/it-travel-web/static/js/runtime~main.d629899f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ittravel-project/it-travel-web/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ittravel-project/it-travel-web/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ittravel-project/it-travel-web/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ittravel-project/it-travel-web/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ittravel-project/it-travel-web/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
