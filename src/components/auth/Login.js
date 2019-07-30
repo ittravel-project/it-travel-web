05cf75d7e0e10083c527b1d0bf6e7c8d29a8d746
RedBalloon24 <worldtraveler757@gmail.com>
2019-07-30 17:58:27 +0200

    style update 2

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import authService from '../../services/AuthService';
-import { withAuthConsumer } from '../../contexts/AuthStore'
+import {withAuthConsumer} from '../../contexts/AuthStore'
 
 // eslint-disable-next-line 
 const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
@@ -26,7 +26,7 @@ const validations = {
   }
 }
 
-class Login extends React.Component {
+class Login extends React.Component{
   state = {
     user: {
       email: '',
@@ -66,8 +66,8 @@ class Login extends React.Component {
     if (this.isValid()) {
       authService.authenticate(this.state.user)
         .then(
-          (user) => this.setState({ isAuthenticated: true }, () => {
-            this.props.onUserChange(user)
+          (user) => this.setState({ isAuthenticated: true }, ()=>{
+              this.props.onUserChange(user)
           }),
           (error) => {
             const { message, errors } = error.response.data;
@@ -89,7 +89,7 @@ class Login extends React.Component {
   }
 
   render() {
-    const { isAuthenticated, errors, user, touch } = this.state;
+    const { isAuthenticated, errors, user, touch } =  this.state;
     if (isAuthenticated) {
       return (<Redirect to="/home" />)
     }
@@ -98,33 +98,33 @@ class Login extends React.Component {
       <div className="container mt-5 login-container">
         <Link to="/"><i className="fa fa-chevron-left"></i></Link>
         <div className="row justify-content-md-center">
-          <div className="col-md-4 login">
-            <h3 className="login-title">Welcome Traveler  <i className="fa fa-globe"></i></h3>
-            <form id="register-form" className="mt-4" onSubmit={this.handleSubmit}>
+          <div className="col-xs-12 col-sm-12 col-md-4 login">
+              <h3 className="login-title">Welcome Traveler  <i className="fa fa-globe"></i></h3>
+              <form id="register-form" className="mt-4" onSubmit={this.handleSubmit}>
               <div className="form-group">
                 <div className="input-group mb-2">
                   <div className="input-group-prepend">
                     <div className="input-group-text login-icons"><i className="fa fa-envelope"></i></div>
                   </div>
                   <input type="email" name="email" className={`shadow form-control ${touch.email && errors.email ? 'is-invalid' : ''}`} onChange={this.handleChange} onBlur={this.handleBlur} value={user.email} placeholder="Email" />
-                  <div className="invalid-feedback">{errors.email}</div>
+                  <div className="invalid-feedback">{ errors.email }</div>
                 </div>
                 <div className="input-group mb-2">
                   <div className="input-group-prepend">
-                    <div className="input-group-text login-icons"><i className="fa fa-lock"></i></div>
+                      <div className="input-group-text login-icons"><i className="fa fa-lock"></i></div>
                   </div>
                   <input type="password" name="password" className={`shadow form-control ${touch.password && errors.password ? 'is-invalid' : ''}`} onChange={this.handleChange} onBlur={this.handleBlur} value={user.password} placeholder="Password" />
-                  <div className="invalid-feedback">{errors.password}</div>
+                  <div className="invalid-feedback">{ errors.password }</div>
                 </div>
               </div>
-              <div className="col-6 login-button">
-                <button className="btn btn-primary shadow" form="register-form" type="submit" disabled={!this.isValid()}>Log in</button>
-              </div>
+                <div className="col-6 login-button">
+                  <button className="btn btn-primary shadow" form="register-form" type="submit" disabled={!this.isValid()}>Log in</button>
+                </div>
             </form>
-            <hr />
-            <a href="http://localhost:3001/authenticate/google" className="btn btn-block btn-danger shadow"><i className="fa fa-google"></i>  Login with Google</a>
-            <a href="http://localhost:3001/authenticate/facebook" className="btn btn-block btn-primary shadow"><i className="fa fa-facebook-f"></i> Login with Facebook</a>
-            <p className="text-center">Not registered? <Link to="/register">Register</Link></p>
+              <hr />
+              <a href="http://localhost:3001/authenticate/google" className="btn btn-block btn-danger shadow"><i className="fa fa-google"></i>  Login with Google</a>
+              <a href="http://localhost:3001/authenticate/facebook" className="btn btn-block btn-primary shadow"><i className="fa fa-facebook-f"></i> Login with Facebook</a>
+              <p className="text-center">Not registered? <Link to="/register">Register</Link></p>
           </div>
         </div>
       </div>
@@ -132,5 +132,5 @@ class Login extends React.Component {
   }
 }
 
-export default withAuthConsumer(Login)
+export default withAuthConsumer(Login) 
 
